f4672fceb5142a4633cc11cdf712cca5b9dfc733
siehun <126447514+siehun@users.noreply.github.com>
2025-06-16 10:18:02 +0800

    IM时间常量

--- y-im-common-domain/src/main/java/io/yue/im/common/domain/constants/IMConstants.java
+++ y-im-common-domain/src/main/java/io/yue/im/common/domain/constants/IMConstants.java
@@ -4,6 +4,9 @@ package io.yue.im.common.domain.constants;
  * IM相关的常量类
  */
 public class IMConstants {
+    /**
+     * =================分隔符相关==========================
+     */
 
     /**
      * 用户id分隔符
@@ -16,6 +19,28 @@ public class IMConstants {
     public static final String REDIS_KEY_SPLIT = ":";
 
     /**
-     *
+     * 发送消息的key
      */
+    public static final String MESSAGE_KEY_SPLIT = "_";
+
+    /**
+     * =====================基本信息相关===========================================
+     */
+
+    /**
+     * 在线状态过期时间，默认10分钟
+     */
+    public static final long ONLINE_TIMEOUT_SECONDS = 600;
+
+    /**
+     * 消息允许撤回时间，默认5分钟
+     */
+    public static final long ALL_RECALL_SECONDS = 300;
+
+
+    /**
+     * 消息的key
+     */
+    public static final String MSG_KEY = "message";
+
 }
