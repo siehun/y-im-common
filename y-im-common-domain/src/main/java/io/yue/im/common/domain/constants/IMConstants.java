165e9cd03206a9eea922f17bc4de179fe18165ea
siehun <126447514+siehun@users.noreply.github.com>
2025-06-16 10:56:43 +0800

    Rocket常量

--- y-im-common-domain/src/main/java/io/yue/im/common/domain/constants/IMConstants.java
+++ y-im-common-domain/src/main/java/io/yue/im/common/domain/constants/IMConstants.java
@@ -39,8 +39,59 @@ public class IMConstants {
 
 
     /**
+     * ===========================Rocket相关的常量==============================
+     */
+
+    /**
      * 消息的key
      */
     public static final String MSG_KEY = "message";
+
+    /**
+     * 未读私聊消息队列
+     */
+    public final static String IM_MESSAGE_PRIVATE_QUEUE = "im_message_private";
+
+    /**
+     * 未读私聊消息空队列
+     */
+    public final static String IM_MESSAGE_PRIVATE_NULL_QUEUE = "im_null_private";
+
+    /**
+     * 监听私聊消息消费分组
+     */
+    public final static String IM_MESSAGE_PRIVATE_CONSUMER_GROUP = "im_message_private_consumer_group";
+    /**
+     * 未读群聊消息队列
+     */
+    public final static String IM_MESSAGE_GROUP_QUEUE = "im_message_group";
+
+    /**
+     * 未读群聊消息空队列
+     */
+    public final static String IM_MESSAGE_GROUP_NULL_QUEUE = "im_null_group";
+
+    /**
+     * 监听群聊消息消费分组
+     */
+    public final static String IM_MESSAGE_GROUP_CONSUMER_GROUP = "im_message_group_consumer_group";
+    /**
+     * 私聊消息发送结果队列
+     */
+    public final static String IM_RESULT_PRIVATE_QUEUE = "im_result_private";
+    /**
+     * 私聊消息结果消费分组
+     */
+    public final static String IM_RESULT_PRIVATE_CONSUMER_GROUP = "im_result_private_consumer_group";
+    /**
+     * 群聊消息发送结果队列
+     */
+    public final static String IM_RESULT_GROUP_QUEUE = "im_result_group";
+
+    /**
+     * 群聊消息结果消费分组
+     */
+    public final static String IM_RESULT_GROUP_CONSUMER_GROUP = "im_result_group_consumer_group";
+
 
 }
