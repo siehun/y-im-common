eb7e3d1166f6b68c34ffbe5a062771ab9d624a15
siehun <126447514+siehun@users.noreply.github.com>
2025-06-16 15:31:15 +0800

    发送状态枚举更正

--- y-im-common-domain/src/main/java/io/yue/im/common/domain/enums/IMSendCode.java
+++ y-im-common-domain/src/main/java/io/yue/im/common/domain/enums/IMSendCode.java
@@ -8,7 +8,7 @@ public enum IMSendCode {
     SUCCESS(0, "发送成功"),
     NOT_ONLINE(1, "对方不在线"),
     NOT_FIND_CHANNEL(2, "未找到对方的channel"),
-    UNKOWN_ERROR(9999, "位置异常");
+    UNKNOW_ERROR(9999, "位置异常");
 
     private final Integer code;
     private final String desc;
