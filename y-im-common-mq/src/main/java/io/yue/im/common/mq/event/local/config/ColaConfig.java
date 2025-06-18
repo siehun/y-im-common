e3bffe8ca1cc113892a20e34ddfedc1a95d6d838
siehun <126447514+siehun@users.noreply.github.com>
2025-06-18 19:41:00 +0800

    cola相关的配置

---
+++ y-im-common-mq/src/main/java/io/yue/im/common/mq/event/local/config/ColaConfig.java
@@ -0,0 +1,20 @@
+package io.yue.im.common.mq.event.local.config;
+
+import com.alibaba.cola.boot.SpringBootstrap;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @description COLA框架的配置
+ */
+@Configuration
+@ComponentScan(value = {"com.alibaba.cola"})
+public class ColaConfig {
+
+    @Bean(initMethod = "init")
+    public SpringBootstrap bootstrap() {
+        SpringBootstrap bootstrap = new SpringBootstrap();
+        return bootstrap;
+    }
+}
